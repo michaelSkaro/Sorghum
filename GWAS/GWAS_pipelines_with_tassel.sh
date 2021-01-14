0abf08ae90a99dcc9780d0b6cbfb6e7db0f7ab11
Michael Skaro <mfs61958@uga.edu>
2021-01-14 11:55:59 -0500

    Create GWAS_pipelines_with_tassel.sh

---
+++ GWAS/GWAS_pipelines_with_tassel.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# author: Michael Skaro
+# date: 1/9/2021
+# purpose: Implement the TASSEL pipeline for QTL and GWAS with MLM and GLM models
+
+
+# check if we need to download and install software if necessary
+
+# return file format as VCF, HapMap, or other
+
+# Complete Diversity analysis
+  # LD
+  # Sequence Diversity
+  
+# Population diversity
+  # PCA
+  #
+
+
+
+
+# DONE
+
